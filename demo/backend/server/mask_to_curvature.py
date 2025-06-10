4dab89e4f691dc9db846c5569e245b2cc76a2a1d
Alex Abbey <alexzabbey@gmail.com>
2025-06-10 15:08:22 +0300

    bug fixes

--- demo/backend/server/mask_to_curvature.py
+++ demo/backend/server/mask_to_curvature.py
@@ -2,7 +2,6 @@ import pandas as pd
 import glob
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from typing import Dict, List, Optional, Tuple
 from numpy.typing import ArrayLike
